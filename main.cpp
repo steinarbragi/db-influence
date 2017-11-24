a50cee62554f3e820cd5b3ca042f42b98cf59ff3
Steinar Bragi Sigurðarson <steinar.bragi@gmail.com>
2017-11-24 10:54:20 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  db-influence
+//
+//  Created by Steinar Bragi Sigurðarson on 11/24/17.
+//  Copyright © 2017 Steinar Bragi Sigurðarson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
